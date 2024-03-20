9053370f94563059911147597c571e2a7712423e
jimmylee/2021 <peteritunuoluwa734@gmail.com>
2024-03-20 06:52:52 -0700

    added more features

--- js/app.js
+++ js/app.js
@@ -16,3 +16,35 @@ menu.addEventListener('click', ()=>{
       menu.classList.remove('active')
    }
 })
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 =  document.getElementById("myBtn-2");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function(e) {
+   e.preventDefault()
+  modal.style.display = "block";
+}
+btn2.onclick = function(e) {
+   e.preventDefault()
+  modal.style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+var cancel = document.getElementById("cancel");
+cancel.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
